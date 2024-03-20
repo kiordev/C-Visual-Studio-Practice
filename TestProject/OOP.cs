a82151e88d4cbcb50b239b2006d88dd30dabb107
kiordev <kiordev@gmail.com>
2024-03-20 11:13:02 +0200

    Добавьте файлы проекта.

---
+++ TestProject/OOP.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!")
+        }
+
+    }
+}
