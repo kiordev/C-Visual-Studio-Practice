3f1d66581505a0de24985af905e573106e5334b6
kiordev <kiordev@gmail.com>
2024-03-20 11:17:36 +0200

    Add new file test

---
+++ TestProject/Testing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Testing
+    {
+        Console.WriteLine("");
+    }
+}
